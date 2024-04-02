b4c0b0b6b518431877945a5c880f617a6fe9cda7
Jaimee22 <alumno.410564@ies-azarquiel.es>
2024-04-02 08:34:41 +0200

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProyectoPDF.Models;
+using System.Diagnostics;
+
+using DinkToPdf;
+using DinkToPdf.Contracts;
+using Microsoft.AspNetCore.Http.Extensions;
+
+namespace ProyectoPDF.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IConverter _converter;
+
+        public HomeController(IConverter converter)
+        {
+           _converter = converter;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult VistaParaPDF()
+        {
+            return View();
+        }
+
+        public IActionResult MostrarPDFenPagina()
+        {
+            string pagina_actual = HttpContext.Request.Path;
+            string url_pagina = HttpContext.Request.GetEncodedUrl();
+            url_pagina = url_pagina.Replace(pagina_actual, "");
+            url_pagina = $"{url_pagina}/Home/VistaParaPDF";
+
+
+            var pdf = new HtmlToPdfDocument()
+            {
+                GlobalSettings = new GlobalSettings() { 
+                    PaperSize = PaperKind.A4,
+                    Orientation = Orientation.Portrait
+                },
+                Objects = { 
+                    new ObjectSettings(){ 
+                        Page = url_pagina
+                    }
+                }
+
+            };
+
+            var  archivoPDF = _converter.Convert(pdf);
+
+
+            return File(archivoPDF, "application/pdf");
+        }
+
+        public IActionResult DescargarPDF()
+        {
+            string pagina_actual = HttpContext.Request.Path;
+            string url_pagina = HttpContext.Request.GetEncodedUrl();
+            url_pagina = url_pagina.Replace(pagina_actual, "");
+            url_pagina = $"{url_pagina}/Home/VistaParaPDF";
+
+
+            var pdf = new HtmlToPdfDocument()
+            {
+                GlobalSettings = new GlobalSettings()
+                {
+                    PaperSize = PaperKind.A4,
+                    Orientation = Orientation.Portrait
+                },
+                Objects = {
+                    new ObjectSettings(){
+                        Page = url_pagina
+                    }
+                }
+
+            };
+
+            var archivoPDF = _converter.Convert(pdf);
+            string nombrePDF = "Alumnos_" + DateTime.Now.ToString("ddMMyyyyHHmmss") + ".pdf";
+
+            return File(archivoPDF, "application/pdf", nombrePDF);
+        }
+
+    }
+}
